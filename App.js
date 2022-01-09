52668622c26f67dcfe6cca15e32be1559c91e221
youngclaude <clauderene@ymail.com>
2022-01-09 00:10:22 -0500

    Added core todo functionality

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  const mochTodos = [
+    'sdfsdfsdfsdf',
+    'dfghfdhh',
+    'sdfsdfscvbnvcbndfsdf',
+    'cvbnvcn',
+  ]
+
+  const handleTodoChange = (givenTodoText) => {
+    setNewTodo(givenTodoText)
+  }
+
+  const handleAddTodo = () => {
+    setTodoList(latestTodoList => [...latestTodoList, newTodo])
+    setNewTodo('')
+  }
+  
+  useEffect(()=> {
+    console.log({todoList});
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.addTodoSection}>
+        <TextInput 
+          placeholder='Enter your goals'
+          style={styles.inputBox}
+          value={newTodo}
+          onChangeText={setNewTodo}
+        />
+        <Button title="Add Todo" onPress={handleAddTodo}/>
+      </View>
+
+
+      <View >
+        {todoList.map( todoName => (
+          <View key={todoName} style={styles.todoItem}>
+            <Text>{todoName}</Text>
+          </View>
+        ) 
+        )}
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +59,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  inputBox: {
+    flex: 0.8,
+    borderWidth: 2,
+    borderColor: 'black',
+    padding: 5
+  },
+  addTodoSection: {
+    flexDirection: 'row'
+  },
+  todoItem: {
+    marginVertical: 10,
+    backgroundColor: '#a3c2c2',
+    minWidth: '80%',
+    flexDirection: 'row',
+    padding: 10 ,
+    // flex: 1
+  }
 });
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React Native ToDoList
-> The purpose of this app is demonstrate a fundamental understanding of React Native via create a ToDoList application
+> The purpose of this app is demonstrate a fundamental understanding of React Native by creating a ToDoList application
 
 ## Key Features
 1. Create todo item
